01a79521608d85967f5004ceaf86f45c8e4e91ae
bruce.mcelroy <bruce.mcelroy@fi360.com>
2018-09-25 13:41:46 -0400

    move post list to its own component

---
+++ components/PostList.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="post-list">
+        <div class="post">
+            <div class="post__meta">
+                <span class="post__category">Design</span> - <span class="post__date">January 5th, 2018</span>
+            </div>
+            <h2 class="post__headline">5 Common Portfolio Mistakes and How to Fix Them</h2>
+            <div class="post__content">Every week users submit a lot of interesting stuff on our sister site Webdesigner News, highlighting great content from around the web that can be of interest to web designers. The best way to keep track of all the great stories..</div>
+            <div class="post__actions">
+                <div class="post__action">
+                    <v-btn flat outline :to="`/blog/${4}`">view post</v-btn>
+                </div>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post__meta">
+                <span class="post__category">Design</span> - <span class="post__date">January 5th, 2018</span>
+            </div>
+            <h2 class="post__headline">5 Common Portfolio Mistakes and How to Fix Them</h2>
+            <div class="post__content">Every week users submit a lot of interesting stuff on our sister site Webdesigner News, highlighting great content from around the web that can be of interest to web designers. The best way to keep track of all the great stories..</div>
+            <div class="post__actions">
+                <div class="post__action">
+                    <v-btn flat outline>view post</v-btn>
+                </div>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post__meta">
+                <span class="post__category">Design</span> - <span class="post__date">January 5th, 2018</span>
+            </div>
+            <h2 class="post__headline">5 Common Portfolio Mistakes and How to Fix Them</h2>
+            <div class="post__content">Every week users submit a lot of interesting stuff on our sister site Webdesigner News, highlighting great content from around the web that can be of interest to web designers. The best way to keep track of all the great stories..</div>
+            <div class="post__actions">
+                <div class="post__action">
+                    <v-btn flat outline>view post</v-btn>
+                </div>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post__meta">
+                <span class="post__category">Design</span> - <span class="post__date">January 5th, 2018</span>
+            </div>
+            <h2 class="post__headline">5 Common Portfolio Mistakes and How to Fix Them</h2>
+            <div class="post__content">Every week users submit a lot of interesting stuff on our sister site Webdesigner News, highlighting great content from around the web that can be of interest to web designers. The best way to keep track of all the great stories..</div>
+            <div class="post__actions">
+                <div class="post__action">
+                    <v-btn flat outline>view post</v-btn>
+                </div>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post__meta">
+                <span class="post__category">Design</span> - <span class="post__date">January 5th, 2018</span>
+            </div>
+            <h2 class="post__headline">5 Common Portfolio Mistakes and How to Fix Them</h2>
+            <div class="post__content">Every week users submit a lot of interesting stuff on our sister site Webdesigner News, highlighting great content from around the web that can be of interest to web designers. The best way to keep track of all the great stories..</div>
+            <div class="post__actions">
+                <div class="post__action">
+                    <v-btn flat outline>view post</v-btn>
+                </div>
+            </div>
+        </div>
+</div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "nuxt-property-decorator";
+
+    @Component({})
+    export default class PostList extends Vue {
+
+    }
+</script>
