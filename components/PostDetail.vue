51ff8f2227cdd0a7613d5bfb3359b2e8ee0f9d81
Bruce McElroy <bruce.mcelroy@Gmail.com>
2018-09-28 19:52:30 -0400

    make post detail padding consistent

--- components/PostDetail.vue
+++ components/PostDetail.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="post-detail">
-        <v-flex py-3>
-                <div class="post-detail__meta">
-                    <span class="post-detail__category">{{post.meta.category}}</span> - <span
-                        class="post-detail__date">{{post.meta.publishDate}}</span>
+        <v-flex py-4>
+            <div class="post-detail__meta">
+                <span class="post-detail__category">{{post.meta.category}}</span> - <span
+                    class="post-detail__date">{{post.meta.publishDate}}</span>
+            </div>
+            <h2 class="post-detail__title headline">{{post.title}}</h2>
+            <div class="post-detail__content" v-html="post.content"></div>
+            <div class="post-detail__actions">
+                <div class="post-detail__action">
+                    <v-btn flat outline color="primary" @click="$router.go(-1)">
+                        <v-icon left>keyboard_arrow_left</v-icon>
+                        Go back
+                    </v-btn>
                 </div>
-                <h2 class="post-detail__title headline">{{post.title}}</h2>
-                <div class="post-detail__content" v-html="post.content"></div>
-                <div class="post-detail__actions">
-                    <div class="post-detail__action">
-                        <v-btn flat outline color="primary" @click="$router.go(-1)">
-                            <v-icon left>keyboard_arrow_left</v-icon>
-                            Go back
-                        </v-btn>
-                    </div>
-                </div>
+            </div>
         </v-flex>
     </div>
 </template>
