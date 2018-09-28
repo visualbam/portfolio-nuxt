489cf45f0b4e767dd4ed16efe2f218a0f145e12c
bruce.mcelroy <bruce.mcelroy@gmail.com>
2018-09-28 13:31:00 -0400

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,10 +39,17 @@ module.exports = {
                 hid: "description",
                 name: "description",
                 content: "Nuxt.js project"
-            }
+            },
+            { name: "msapplication-TileColor", content: "#da532c" },
+            { name: "theme-color", content: "#ffffff" }
         ],
         link: [
-            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "apple-touch-icon", sizes: "180x180", type: "image/x-icon", href: "/apple-touch-icon.png" },
+            { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+            { rel: "icon", type: "image/png", sizes: "16x16",  href: "/favicon-16x16.png" },
+            { rel: "manifest", href: "/site.webmanifest" },
+            { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
         ]
     },
     /*
